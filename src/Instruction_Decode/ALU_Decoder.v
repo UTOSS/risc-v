db61d47e5d925c15e99e14e75ecffc3be43f8352
Joonseo Park <90308427+joon2022park@users.noreply.github.com>
2025-02-02 14:43:47 -0500

    Update ALU_Decoder.v
    Update ALU Decoder RTL code with Daniel's refactor

--- src/Instruction_Decode/ALU_Decoder.v
+++ src/Instruction_Decode/ALU_Decoder.v
@@ -1,48 +1,21 @@
-module ALU_Decoder(
+module ALUdecoder (input [2:0] funct3, input [6:0] funct7, output reg [3:0] alu_op);
 	
-	input wire [1:0] ALUOp,
-	input wire [2:0] funct3,
-	input wire opcode5,
-	input wire funct7_5,
-	output reg [2:0] ALUControl
-
-);
-	
-	wire opfunct7 = {opcode5, funct7_5};
-
-	always@(*) begin
-	
-		if (ALUOp == 2'b00) ALUControl = 3'b000;
-		
-		else if (ALUOp == 2'b01) ALUControl = 3'b001;
-		
-		else if (ALUOp == 2'b10) begin
-		
-			case (funct3)
-			
-				3'b000: begin
-				
-					if (opfunct7 == 2'b00 || opfunct7 == 2'b01 || opfunct7 == 2'b10) 
-						ALUControl = 3'b000;
-					
-					else ALUControl = 3'b001;
-				
-				end
-				
-				3'b010: ALUControl = 3'b101;
-				
-				3'b110: ALUControl = 3'b011;
-				
-				3'b111: ALUControl = 3'b010;
-				
-				default: ALUControl  = 3'b000;
-			
-			endcase
-		
-		end
-		
-		else ALUControl = 3'b000;
-	
+	always @(*)
+	begin
+		case (funct3)
+			3'b000:
+				if (funct7==7'h00) alu_op = 4'b0000;      //ADD
+				else if (funct7==7'h20) alu_op = 4'b0001; //SUB
+			3'b001: alu_op = 4'b0010;                         //SLL
+			3'b010: alu_op = 4'b0011;                         //SLT
+			3'b011: alu_op = 4'b0100;                         //SLTU
+			3'b100: alu_op = 4'b0101;						  //XOR
+			3'b101:
+				if (funct7==7'h00) alu_op = 4'b0110;      //SRL
+				else if (funct7==7'h20) alu_op = 4'b0111; //SRA
+			3'b110: alu_op = 4'b1000;                         //OR
+			3'b111: alu_op = 4'b1001;                         //AND
+			default: alu_op = 4'b0;
+		endcase
 	end
-
 endmodule
