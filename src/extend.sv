ac119dbbe6b68bc81c327f8d488c2db7bfffe737
Boris <boris.potapov@mail.utoronto.ca>
2025-03-13 01:38:54 +0000

    another pass at branching

--- src/extend.sv
+++ src/extend.sv
@@ -1,6 +1,9 @@
 // dummy extend module
+
+`include "src/types.svh"
+
 module extend ( input  wire [11:0] in
-              , output wire [31:0] imm_ext
+              , output imm_t       imm_ext
               );
   assign imm_ext = {{20{in[11]}}, in};
 endmodule
