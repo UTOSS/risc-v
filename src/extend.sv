592cbff635b8592aea50a297f678d8eb618106ea
Boris <boris.potapov@mail.utoronto.ca>
2025-05-27 12:40:25 +0000

    remove extend module

--- src/extend.sv
+++
@@ -1,9 +0,0 @@
-// dummy extend module
-
-`include "src/types.svh"
-
-module extend ( input  wire [11:0] in
-              , output imm_t       imm_ext
-              );
-  assign imm_ext = {{20{in[11]}}, in};
-endmodule
