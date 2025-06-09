925cc8ad5b41492c09bbff7476398b585f9f3215
MM871 <100428910+MM871@users.noreply.github.com>
2025-06-09 18:25:28 -0400

    Update top.v
    instantiated MA and ID

--- src/top.v
+++ src/top.v
@@ -1,34 +1,71 @@
-module InstructionDecoder(
+`include "src/types.svh"
 
-    output [6:0]  op,     
-    output [2:0]  funct3, 
-    output [6:0]  funct7, 
-    output [4:0]  rs1, 
-    output [4:0] rs2.
-    output [4:0] rd, 
-    output [24:0] extend,
-    input [31:0] instr
+module top ( input wire clk
+           , input wire reset
+           );
 
-);
+  wire cfsm__pc_update;
+  instr_t instr;
 
-assign op = instr[6:0];
-assign rd = instr[11:7];
-assign funct3 = instr[14:12];
-assign rs1 = instr[19:15];
-assign rs2 = instr[24:20];
-assign funct7 = instr[31:25];
-assign extend = instr[31:7];
+  wire __tmp_AdrSrc
+     , __tmp_IRWrite
+     , __tmp_RegWrite
+     , __tmp_MemWrite
+     , __tmp_Branch;
+  wire [1:0] __tmp_ALUSrcA
+           , __tmp_ALUSrcB;
+  wire [2:0] __tmp_ALUOp;
+  wire [1:0] __tmp_ResultSrc;
+  wire [3:0] __tmp_FSMState;
 
+  ControlFSM control_fsm
+    ( .opcode    ( 7'b0000000      )
+    , .clk       ( clk             )
+    , .reset     ( reset           )
+    , .AdrSrc    ( __tmp_AdrSrc    )
+    , .IRWrite   ( __tmp_IRWrite   )
+    , .RegWrite  ( __tmp_RegWrite  )
+    , .PCUpdate  ( cfsm__pc_update )
+    , .MemWrite  ( __tmp_MemWrite  )
+    , .Branch    ( __tmp_Branch    )
+    , .ALUSrcA   ( __tmp_ALUSrcA   )
+    , .ALUSrcB   ( __tmp_ALUSrcB   )
+    , .ALUOp     ( __tmp_ALUOp     )
+    , .ResultSrc ( __tmp_ResultSrc )
+    , .FSMState  ( __tmp_FSMState  )
+    );
 
-InstructionDecoder instance1 (
+  fetch fetch
+    ( .clk             ( clk             )
+    , .reset           ( reset           )
+    , .cfsm__pc_update ( cfsm__pc_update )
+    , .instr           ( instr           )
+    );
 
-    .instr(instr),
-    .rd(rd),
-    .funct3(funct3),
-    .rs1(rs1),
-    .rs2(rs2),
-    .funct7(funct7),
-    .extend(extend)
-);
+  MA memory_access (
+      .A   (baseAddr),        //baseAddr is from Instruction_Decode.v it sounded like it matches with the module paramters in MA
+      .WD  (writeData),       //same as above
+      .WE  (MemWrite),        //same as above but from ControlFSM interacting with MA e.g. when memWrite = 1
+      .CLK (clk),
+      .RD  (ma__readdata)     //just left this because i dont think its connected to anything ?
+  );
+
+  Instruction_Decode instruction_decode (
+      .instr      (instr),
+      .clk        (clk),
+      .reset      (reset),
+      .ResultData (ResultData),   
+      .AdrSrc     (AdrSrc),
+      .IRWrite    (IRWrite),
+      .PCUpdate   (PCUpdate),     
+      .MemWrite   (MemWrite),
+      .Branch     (Branch),
+      .ALUSrcA    (ALUSrcA),
+      .ALUSrcB    (ALUSrcB),
+      .ResultSrc  (ResultSrc),
+      .ALUControl (ALUControl),
+      .baseAddr   (baseAddr),     
+      .writeData  (writeData)   
+  );
 
 endmodule
