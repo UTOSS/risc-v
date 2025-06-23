dea7059c4e8aab8c72bc4e8f8e6cc5f8a70284dc
Joonseo Park <90308427+joon2022park@users.noreply.github.com>
2025-06-23 14:36:46 -0700

    Add Reset To RegFile_TB.sv
    Added:
    
    A reset at the start to ensure r0 = 0
    Improved display statements outputting register file outputs/contents

--- src/Instruction_Decode/RegFile_TB.sv
+++ src/Instruction_Decode/RegFile_TB.sv
@@ -7,6 +7,7 @@ module tb_registerFile;
   logic clk;
   logic regWrite;
   logic [31:0] dataIn;
+  logic reset;
 
   //DUT outputs
   logic [31:0] baseAddr, writeData;
@@ -19,6 +20,7 @@ module tb_registerFile;
     .clk(clk),
     .regWrite(regWrite),
     .dataIn(dataIn),
+    .reset(reset),
     .baseAddr(baseAddr),
     .writeData(writeData)
   );
@@ -29,6 +31,12 @@ module tb_registerFile;
   initial begin
     $display("Starting Register File Testbench...");
     clk = 0;
+
+    //RESET REGISTER FILE
+    reset = 1;
+    @(posedge clk); //waits for positive clock edge
+    #1; // wait for output to stabilize after posedge
+    reset = 0;
 
     // STEP 0: Manual initialization of RF memory (reg 5 and 10)
     dut.RFMem[5] = 32'hDEADBEEF; //preloads memory values into registers
@@ -44,8 +52,10 @@ module tb_registerFile;
 
     @(posedge clk); //waits for positive clock edge
     #1; // wait for output to stabilize after posedge
-    assert(baseAddr == 32'hDEADBEEF) else $fatal("current baseAddr output: %h; expected baseAddr output: DEADBEEF", baseAddr);
-    assert(writeData == 32'hCAFEBABE) else $fatal("current writeData output: %h; expected writeData output: CAFEBABE", writeData);
+    assert(baseAddr == 32'hDEADBEEF) else $fatal(1, "READ FAILED: current baseAddr output: %h; expected baseAddr output: DEADBEEF", baseAddr);
+    assert(writeData == 32'hCAFEBABE) else $fatal(1, "READ FAILED: current writeData output: %h; expected writeData output: CAFEBABE", writeData);
+    $display("PASS: current baseAddr output: %h; expected baseAddr output: DEADBEEF", baseAddr);
+    $display("PASS: current writeData output: %h; expected writeData output: CAFEBABE", writeData);
 
     //CASE 2 - Write to reg 15
     Addr3 = 15;
@@ -53,22 +63,22 @@ module tb_registerFile;
     regWrite = 1;
  
     @(posedge clk); //wait one clock cycle
-    regWrite = 0; //de-assert write
 
     #1; //wait for written data to stabilize
-    assert(dut.RFMem[15] == 32'h12345678) else $fatal("WRITE FAILED: current reg 15 output: %h; expected reg 15 output: 12345678", dut.RFMem[15]);
-    $display("PASS: Write to register verified");
+    regWrite = 0; //de-assert write
+    assert(dut.RFMem[15] == 32'h12345678) else $fatal(1, "WRITE FAILED: current reg 15 output: %h; expected reg 15 output: 12345678", dut.RFMem[15]);
+    $display("PASS: Write to register 15 verified. current reg 15 output: %h; expected reg 15 output: 12345678", dut.RFMem[15]);
 	 
 	 //CASE 3 - Write to reg 0
 	 Addr3 = 0;
 	 dataIn = 32'h12345678;
 	 
 	 @(posedge clk); //wait one clock cycle
-	 regWrite = 0; //de-assert write
 	 
     #1; //wait for written data to stabilize
-    assert(dut.RFMem[0] == 0) else $fatal("WRITE FAILED: current reg 0 output: %h; expected reg 15 output: 0", dut.RFMem[0]);	 
-    $display("PASS: Write to register verified");
+    regWrite = 0; //de-assert write
+    assert(dut.RFMem[0] == 0) else $fatal(1, "WRITE FAILED: current reg 0 output: %h; expected reg 0 output: 0", dut.RFMem[0]);	 
+    $display("PASS: Write to register 0 verified. current reg 0 output: %h; expected reg 0 output: 0", dut.RFMem[0]);
 
     $finish;
   end
