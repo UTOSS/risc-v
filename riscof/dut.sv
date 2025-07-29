3e9a27e794cac26aacce5b0b54c9f631ca9471c2
Boris <boris.potapov@mail.utoronto.ca>
2025-07-29 00:14:54 +0000

    extend timeout

--- riscof/dut.sv
+++ riscof/dut.sv
@@ -53,7 +53,7 @@ module dut;
 
   task watch_timeout();
     $display("%m: waiting for timeout...");
-    repeat (1) @(posedge clk);
+    repeat (100) @(posedge clk);
     $display("%m: timeout reached");
     void'(extract_signature());
   endtask
