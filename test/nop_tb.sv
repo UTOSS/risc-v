2804c8e693a677a9b0f4d6e7ca0e9b370e5653a0
Boris <boris.potapov@mail.utoronto.ca>
2025-09-22 01:05:38 +0000

    fix nop tb

--- test/nop_tb.sv
+++ test/nop_tb.sv
@@ -2,13 +2,13 @@
 
 `include "test/utils.svh"
 
-//generate clock (10ns period)
-initial begin
-    clk = 0;
-    forever #5 clk = ~clk;
-end
-
 module nop_tb;
+
+    reg clk;
+    reg reset;
+
+    //instantiate top module
+    top uut ( .clk(clk), .reset(reset) );
 
     //helper task to compare current state with the expected current state
     task wait_till_next_cfsm_state(input [5:0] expected_state);
@@ -16,42 +16,50 @@ module nop_tb;
         `assert_equal(uut.control_fsm.current_state, expected_state)
     endtask
 
-    //instantiate top module
-    top cpucore ( .clk(clk), .reset(reset));
+    //generate clock (10ns period)
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
 
     initial begin
-    
+
         reset <= `TRUE;
 
         //pre-write NOP instruction into instruction memory
-        cpucore.memory.M[0] = 32'h00000013; //NOP instruction - addi x0, x0, 0
+        uut.memory.M[0] = 32'h00000013; //NOP instruction - addi x0, x0, 0
+
+        // set up register file to a known value
+        uut.instruction_decode.instanceRegFile.RFMem[0] = 32'h01010101; // x0
 
         //wait until reset makes FSM go to fetch state
-        wait_till_next_cfsm_state(cpucore.control_fsm.FETCH);
+        wait_till_next_cfsm_state(uut.control_fsm.FETCH);
 
         reset <= `FALSE;
 
-        wait_till_next_cfsm_state(cpucore.control_fsm.DECODE);
+        wait_till_next_cfsm_state(uut.control_fsm.DECODE);
 
-        `assert_equal(cpucore.opcode, 7'b0010011)
-        `assert_equal(cpucore.instruction_decode.rs1, 0)
-        `assert_equal(cpucore.instruction_decode.rd, 0)
-        `assert_equal(cpucore.instruction_decode.imm_ext, 0)
+        `assert_equal(uut.opcode, 7'b0010011)
+        `assert_equal(uut.instruction_decode.rs1, 0)
+        `assert_equal(uut.instruction_decode.rd, 0)
+        `assert_equal(uut.instruction_decode.imm_ext, 0)
 
         wait_till_next_cfsm_state(uut.control_fsm.EXECUTEI);
-        `assert_equal(uut.alu.a, 0)
+        `assert_equal(uut.alu.a, 32'h01010101)
         `assert_equal(uut.alu.b, 0)
-        `assert_equal(uut.alu.out, 0)
+        `assert_equal(uut.alu.out, 32'h01010101)
 
         wait_till_next_cfsm_state(uut.control_fsm.ALUWB);
 
         wait_till_next_cfsm_state(uut.control_fsm.FETCH);
-        `assert_equal(uut.instruction_decode.instanceRegFile.RFMem[0], 0)
-        `assert_equal(uut.fetch.pc_cur, 8)
+        `assert_equal(uut.instruction_decode.instanceRegFile.RFMem[0], 32'h01010101)
+        `assert_equal(uut.fetch.pc_cur, 4)
+
+        $finish;
 
     end
 
-    `SETUP_VCD_DUMP(or_tb)
+    `SETUP_VCD_DUMP(nop_tb)
 
 
 endmodule
