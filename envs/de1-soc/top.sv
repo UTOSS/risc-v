1da5427fcb95a570b77383361163d19942f55adf
Boris <boris.potapov@mail.utoronto.ca>
2025-10-31 00:15:14 +0000

    update memory map for DE1-SoC env

--- envs/de1-soc/top.sv
+++ envs/de1-soc/top.sv
@@ -9,18 +9,26 @@ module top
   logic  memory__write_enable;
   data_t memory__read_data;
 
-  MA #( .SIZE ( 8192 ) )
-    memory
-      ( .clk          ( clk                  )
+  memory_map #( .SIZE ( 8192 ) )
+    memory_map
+      ( .clk ( clk )
+
       , .address      ( memory__address      )
       , .write_data   ( memory__write_data   )
       , .write_enable ( memory__write_enable )
       , .read_data    ( memory__read_data    )
+
+      , .LEDR ( LEDR )
       );
 
   utoss_riscv core
     ( .clk   ( CLOCK_50 )
     , .reset ( KEY[0]   )
+
+    , .memory__address      ( memory__address      )
+    , .memory__write_data   ( memory__write_data   )
+    , .memory__write_enable ( memory__write_enable )
+    , .memory__read_data    ( memory__read_data    )
     );
 
 endmodule
