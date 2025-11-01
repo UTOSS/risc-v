93ea04d7e23ec498ce50466012dd6afa16fcc4e8
Boris <boris.potapov@mail.utoronto.ca>
2025-11-01 21:13:06 +0000

    fix simple infraction

--- src/Instruction_Decode/MemoryLoader.sv
+++ src/Instruction_Decode/MemoryLoader.sv
@@ -8,9 +8,9 @@ module MemoryLoader
     integer byteindex;
     assign byteindex = memory_address[1:0];
 
-    always_comb begin
+    always_comb
         case (funct3)
-            3'b000: begin // lb
+            3'b000: // lb
                 case (byteindex)
                     2'd0: mem_load_result = {{24{memory_data[7]}} , memory_data[7:0]};
                     2'd1: mem_load_result = {{24{memory_data[15]}}, memory_data[15:8]};
@@ -18,20 +18,18 @@ module MemoryLoader
                     2'd3: mem_load_result = {{24{memory_data[31]}}, memory_data[31:24]};
                     default: mem_load_result = 32'hX;
                 endcase
-            end
 
-            3'b001: begin // lh
+            3'b001: // lh
                 case (byteindex)
                     2'd0: mem_load_result = {{16{memory_data[15]}}, memory_data[15:0]};
                     2'd1: mem_load_result = {{16{memory_data[23]}}, memory_data[23:8]};
                     2'd2: mem_load_result = {{16{memory_data[31]}}, memory_data[31:16]};
                     default: mem_load_result = 32'hX;
                 endcase
-            end
 
             3'b010: mem_load_result = memory_data; // lw
 
-            3'b100: begin // lbu
+            3'b100: // lbu
                 case (byteindex)
                     2'd0: mem_load_result = {24'b0, memory_data[7:0]};
                     2'd1: mem_load_result = {24'b0, memory_data[15:8]};
@@ -39,19 +37,16 @@ module MemoryLoader
                     2'd3: mem_load_result = {24'b0, memory_data[31:24]};
                     default: mem_load_result = 32'hX;
                 endcase
-            end
 
-            3'b101: begin // lhu
+            3'b101: // lhu
                 case (byteindex)
                     2'd0: mem_load_result = {16'b0, memory_data[15:0]};
                     2'd1: mem_load_result = {16'b0, memory_data[23:8]};
                     2'd2: mem_load_result = {16'b0, memory_data[31:16]};
                     default: mem_load_result = 32'hX;
                 endcase
-            end
 
             default: mem_load_result = 32'hX;
         endcase
-    end
 
 endmodule
