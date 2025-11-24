fb04b5adbff58789e86346a3c1a742fefc231f4e
Boris <boris.potapov@mail.utoronto.ca>
2025-11-24 21:18:38 +0000

    set up a 5MHz clock to rectify timing issues

--- envs/de1-soc/top.sv
+++ envs/de1-soc/top.sv
@@ -4,6 +4,19 @@ module top
   , output wire [9:0] LEDR
   );
 
+  // Clock division: 50MHz to 5MHz (divide by 10)
+  reg [3:0] clk_counter = 0;
+  reg clk_5mhz = 0;
+
+  always @(posedge CLOCK_50) begin
+    if (clk_counter == 4'd9) begin
+      clk_counter <= 0;
+      clk_5mhz <= ~clk_5mhz;
+    end else begin
+      clk_counter <= clk_counter + 1;
+    end
+  end
+
   addr_t       memory__address;
   data_t       memory__write_data;
   logic  [3:0] memory__write_enable;
@@ -11,7 +24,7 @@ module top
 
   memory_map #( .SIZE ( 512 ) )
     memory_map
-      ( .clk ( CLOCK_50 )
+      ( .clk ( clk_5mhz )
 
       , .address      ( memory__address      )
       , .write_data   ( memory__write_data   )
@@ -22,7 +35,7 @@ module top
       );
 
   utoss_riscv core
-    ( .clk   ( CLOCK_50 )
+    ( .clk   ( clk_5mhz )
     , .reset ( KEY[0]   )
 
     , .memory__address      ( memory__address      )
