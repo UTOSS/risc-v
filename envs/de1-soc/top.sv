d94f5b6a9213bd95a7d4fdbaa332769e4ebd86d3
Boris <boris.potapov@mail.utoronto.ca>
2025-11-25 00:33:52 +0000

    drop the clock division

--- envs/de1-soc/top.sv
+++ envs/de1-soc/top.sv
@@ -4,19 +4,6 @@ module top
   , output wire [9:0] LEDR
   );
 
-  // Clock division: 50MHz to 5MHz (divide by 10)
-  reg [3:0] clk_counter = 0;
-  reg clk_5mhz = 0;
-
-  always @(posedge CLOCK_50) begin
-    if (clk_counter >= 4'd5) begin
-      clk_counter <= 0;
-      clk_5mhz <= ~clk_5mhz;
-    end else begin
-      clk_counter <= clk_counter + 1;
-    end
-  end
-
   addr_t       memory__address;
   data_t       memory__write_data;
   logic  [3:0] memory__write_enable;
@@ -24,7 +11,7 @@ module top
 
   memory_map #( .SIZE ( 512 ) )
     memory_map
-      ( .clk ( clk_5mhz )
+      ( .clk ( CLOCK_50 )
 
       , .address      ( memory__address      )
       , .write_data   ( memory__write_data   )
@@ -35,7 +22,7 @@ module top
       );
 
   utoss_riscv core
-    ( .clk   ( clk_5mhz )
+    ( .clk   ( CLOCK_50 )
     , .reset ( KEY[0]   )
 
     , .memory__address      ( memory__address      )
