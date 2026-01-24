d98e1d5fe33a5299002577ef6888eecc168bbfce
RunzeZhu28 <126600309+RunzeZhu28@users.noreply.github.com>
2026-01-24 00:16:06 -0500

    lint fixing

--- envs/de1-soc/top.sv
+++ envs/de1-soc/top.sv
@@ -25,24 +25,20 @@ module top (
 			 .BAUD(115200)
 	 ) 
 	 u_uart (
-        .clk(CLOCK_50),
-        .rst(rst),
-
-        .i_data_s(uart_tx_data),
-        .i_valid_s(uart_tx_valid),
-        .o_ready_s(uart_tx_ready),
-
-        .o_data_m(uart_rx_data),
-        .o_valid_m(uart_rx_valid),
-        .i_ready_m(uart_rx_ready),
-
-        .i_rxd(UART_TX),
-        .o_txd(UART_RX),
-
-        .o_tx_busy(tx_busy),
-        .o_rx_busy(rx_busy),
-        .o_rx_overrun_error(rx_overrun),
-        .o_rx_frame_error(rx_frame)
+        .clk(CLOCK_50)
+        , .rst(rst)
+        , .i_data_s(uart_tx_data)
+        , .i_valid_s(uart_tx_valid)
+        , .o_ready_s(uart_tx_ready)
+        , .o_data_m(uart_rx_data)
+        , .o_valid_m(uart_rx_valid)
+        , .i_ready_m(uart_rx_ready)
+        , .i_rxd(UART_TX)
+        , .o_txd(UART_RX)
+        , .o_tx_busy(tx_busy)
+        , .o_rx_busy(rx_busy)
+        , .o_rx_overrun_error(rx_overrun)
+        , .o_rx_frame_error(rx_frame)
     );
 
 
@@ -53,24 +49,21 @@ module top (
     logic hold_core;
 
     uart_bus_master u_master (
-        .clk(CLOCK_50),
-        .rst(rst),
-
-        .rx_data(uart_rx_data),
-        .rx_valid(uart_rx_valid),
-        .rx_ready(uart_rx_ready),
-
-        .tx_data(uart_tx_data),
-        .tx_valid(uart_tx_valid),
-        .tx_ready(uart_tx_ready),
-
-        .bus_addr(dbg_addr),
-        .bus_write_data(dbg_write_data),
-        .bus_write_enable(dbg_write_enable),
-        .bus_read_data(read_data),
-		  .dbg_regs(dbg_regs),
-		  .dbg_pc(dbg_pc),
-        .hold_core(hold_core)
+        .clk(CLOCK_50)
+        , .rst(rst)
+        , .rx_data(uart_rx_data)
+        , .rx_valid(uart_rx_valid)
+        , .rx_ready(uart_rx_ready)
+        , .tx_data(uart_tx_data)
+        , .tx_valid(uart_tx_valid)
+        , .tx_ready(uart_tx_ready)
+        , .bus_addr(dbg_addr)
+        , .bus_write_data(dbg_write_data)
+        , .bus_write_enable(dbg_write_enable)
+        , .bus_read_data(read_data)
+        , .dbg_regs(dbg_regs)
+        , .dbg_pc(dbg_pc)
+        , .hold_core(hold_core)
     );
 
     addr_t core_addr;
@@ -94,27 +87,25 @@ module top (
 			.SIZE(512) 
 	 ) 
 	 u_mem (
-        .clk(CLOCK_50),
-        .address(bus_addr),
-        .write_data(bus_write_data),
-        .write_enable(bus_write_enable),
-        .read_data(read_data),
-        .dbg_regs(dbg_regs),
-        .dbg_pc(dbg_pc),
-        .LEDR(LEDR)
+        .clk(CLOCK_50)
+        , .address(bus_addr)
+        , .write_data(bus_write_data)
+        , .write_enable(bus_write_enable)
+        , .read_data(read_data)
+        , .dbg_regs(dbg_regs)
+        , .dbg_pc(dbg_pc)
+        , .LEDR(LEDR)
     );
 
     utoss_riscv core (
-        .clk(CLOCK_50),
-        .reset(core_reset),
-
-        .memory__address(core_addr),
-        .memory__write_data(core_write_data),
-        .memory__write_enable(core_write_enable),
-        .memory__read_data(read_data),
-
-        .dbg_regs(dbg_regs),
-        .dbg_pc(dbg_pc)
+        .clk(CLOCK_50)
+        , .reset(core_reset)
+        , .memory__address(core_addr)
+        , .memory__write_data(core_write_data)
+        , .memory__write_enable(core_write_enable)
+        , .memory__read_data(read_data)
+        , .dbg_regs(dbg_regs)
+        , .dbg_pc(dbg_pc)
     );
 
 endmodule
