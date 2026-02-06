dfa1b58595249ff374007964e50cbd80b58bc69c
Boris <boris.potapov@mail.utoronto.ca>
2026-02-06 05:02:33 +0000

    clock if->id state

--- src/utoss_riscv_pipelined.sv
+++ src/utoss_riscv_pipelined.sv
@@ -22,7 +22,8 @@ module utoss_riscv_pipelined
 
   // common declarations begin
 
-  if_to_id_if  if_to_id_if();
+  if_to_id_if  if_to_id_out_if();
+  if_to_id_if  if_to_id_reg_if();
   id_to_ex_if  id_to_ex_if();
   mem_to_wb_if mem_to_wb_if();
 
@@ -36,17 +37,24 @@ module utoss_riscv_pipelined
   // fetch stage end
 
   // decode stage begin (@marwannismail)
+
+  // TODO: this will blow up on us for large interfaces; i think we might need to use packed structs
+  // to make cross-stage register assignments more elegant
+  always_ff @ (posedge clk)
+    if (reset) if_to_id_reg_if.instruction <= '0;
+    else       if_to_id_reg_if.instruction <= if_to_id_out_if.instruction;
+
   // TODO: move zero flag to Fetch stage
   // TODO: remove ALU result from control FSM (should not be added to Fetch stage according to the draw.io diagram note)
   Decode decode
-    ( .IF_to_ID    (  if_to_id_if  )
-    , .clk         (  clk          )
-    , .reset       (  reset        )
-    , .data        (  wb_result    )
-    , .rd_wb       (  wb_rd        )
-    , .zero_flag   (  zero_flag    )
-    , .alu_result  (  alu_result   )
-    , .ID_to_EX    (  id_to_ex_if  )
+    ( .IF_to_ID    ( if_to_id_reg_if )
+    , .clk         ( clk             )
+    , .reset       ( reset           )
+    , .data        ( wb_result       )
+    , .rd_wb       ( wb_rd           )
+    , .zero_flag   ( zero_flag       )
+    , .alu_result  ( alu_result      )
+    , .ID_to_EX    ( id_to_ex_if     )
     );
 
   // decode stage end
