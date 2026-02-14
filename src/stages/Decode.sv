1d2a38b77d773b98ec3502303282ce293ab0db78
Boris <boris.potapov@mail.utoronto.ca>
2026-02-14 05:08:50 +0000

    make decode state assignment combinational

--- src/stages/Decode.sv
+++ src/stages/Decode.sv
@@ -87,54 +87,27 @@ module Decode
     , .writeData       ( rd2              )
     );
 
-    always @(posedge clk)
-    if (reset) begin
-        ID_to_EX.ALUSrcA <= alu_src_a_t'('0);
-        ID_to_EX.ALUSrcB <= alu_src_b_t'('0);
-        ID_to_EX.ResultSrc <= result_src_t'('0);
-        ID_to_EX.AdrSrc <= adr_src_t'('0);
-        ID_to_EX.pc_update <= 'b0;
-        ID_to_EX.pc_src <= pc_src_t'('0);
-        ID_to_EX.IRWrite <= 'b0;
-        ID_to_EX.Branch <= 'b0;
-        ID_to_EX.MemWriteByteAddress <= 'b0;
-        ID_to_EX.FSMState <= 'b0;
-        ID_to_EX.MemWrite <= 'b0;
-        ID_to_EX.RegWrite <= 'b0;
-        ID_to_EX.ALUControl <= 'b0;
-        ID_to_EX.funct3 <= 'b0;
-        ID_to_EX.funct7 <= 'b0;
-        ID_to_EX.rd1 <= 'b0;
-        ID_to_EX.rd2 <= 'b0;
-        ID_to_EX.rd <= 'b0;
-        ID_to_EX.rs1 <= 'b0;
-        ID_to_EX.rs2 <= 'b0;
-        ID_to_EX.imm_ext <= 'b0;
-        ID_to_EX.pc_cur <= 'b0;
-    end
-    else begin
-        ID_to_EX.ALUSrcA <= __tmp_ALUSrcA;
-        ID_to_EX.ALUSrcB <= __tmp_ALUSrcB;
-        ID_to_EX.ResultSrc <= cfsm__result_src;
-        ID_to_EX.AdrSrc <= cfsm__adr_src;
-        ID_to_EX.pc_update <= cfsm__pc_update;
-        ID_to_EX.pc_src <= cfsm__pc_src;
-        ID_to_EX.IRWrite <= cfsm__ir_write;
-        ID_to_EX.Branch <= __tmp_Branch;
-        ID_to_EX.MemWriteByteAddress <= MemWriteByteAddress;
-        ID_to_EX.FSMState <= __tmp_FSMState;
-        ID_to_EX.MemWrite <= __tmp_MemWrite;
-        ID_to_EX.RegWrite <= cfsm__reg_write;
-        ID_to_EX.ALUControl <= __tmp_ALUControl;
-        ID_to_EX.funct3 <= funct3;
-        ID_to_EX.funct7 <= funct7;
-        ID_to_EX.rd1 <= rd1;
-        ID_to_EX.rd2 <= rd2;
-        ID_to_EX.rd <= rd;
-        ID_to_EX.rs1 <= rs1;
-        ID_to_EX.rs2 <= rs2;
-        ID_to_EX.imm_ext <= imm_ext;
-        ID_to_EX.pc_cur <= IF_to_ID.pc_cur;
-    end
+    assign ID_to_EX.ALUSrcA             = __tmp_ALUSrcA;
+    assign ID_to_EX.ALUSrcB             = __tmp_ALUSrcB;
+    assign ID_to_EX.ResultSrc           = cfsm__result_src;
+    assign ID_to_EX.AdrSrc              = cfsm__adr_src;
+    assign ID_to_EX.pc_update           = cfsm__pc_update;
+    assign ID_to_EX.pc_src              = cfsm__pc_src;
+    assign ID_to_EX.IRWrite             = cfsm__ir_write;
+    assign ID_to_EX.Branch              = __tmp_Branch;
+    assign ID_to_EX.MemWriteByteAddress = MemWriteByteAddress;
+    assign ID_to_EX.FSMState            = __tmp_FSMState;
+    assign ID_to_EX.MemWrite            = __tmp_MemWrite;
+    assign ID_to_EX.RegWrite            = cfsm__reg_write;
+    assign ID_to_EX.ALUControl          = __tmp_ALUControl;
+    assign ID_to_EX.funct3              = funct3;
+    assign ID_to_EX.funct7              = funct7;
+    assign ID_to_EX.rd1                 = rd1;
+    assign ID_to_EX.rd2                 = rd2;
+    assign ID_to_EX.rd                  = rd;
+    assign ID_to_EX.rs1                 = rs1;
+    assign ID_to_EX.rs2                 = rs2;
+    assign ID_to_EX.imm_ext             = imm_ext;
+    assign ID_to_EX.pc_cur              = IF_to_ID.pc_cur;
 
 endmodule
