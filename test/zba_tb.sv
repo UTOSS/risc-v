9e3228b8e73f9a2e15b4f89140faff79c91eb65f
Russell Tabata <61029634+Parzival129@users.noreply.github.com>
2026-02-20 07:25:53 +0000

    Made svlint changes for zba_tb.sv and utilized vcd dump macros

--- test/zba_tb.sv
+++ test/zba_tb.sv
@@ -1,4 +1,5 @@
 `timescale 1ns/1ns
+`include "test/utils.svh"
 
 module zba_tb;
 
@@ -7,16 +8,13 @@ reg[31:0] reg2;
 reg[1:0] inst;
 wire[31:0] out;
 
-zba uut(
-    .reg1(reg1), 
-    .reg2(reg2), 
-    .inst(inst), 
-    .out(out));
+zba uut(.reg1(reg1)
+  , .reg2(reg2)
+  , .inst(inst)
+  , .out(out)
+  );
 
 initial begin
-
-    $dumpfile("zba.vcd");
-    $dumpvars(0, zba_tb);
 
     reg1 = 32'd10; reg2 = 31'd5; inst=2'b00; #20;
     reg1 = 32'd10; reg2 = 31'd5; inst=2'b01; #20;
@@ -25,5 +23,7 @@ initial begin
     $finish;
 
 end
+
+`SETUP_VCD_DUMP(zba_tb)
 
 endmodule
