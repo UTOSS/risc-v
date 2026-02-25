2fd4220089721a4e5a0dfbdf6128f65283286818
Russell Tabata <61029634+Parzival129@users.noreply.github.com>
2026-02-25 04:13:28 +0000

    Updated b extension decoder to use enum type instead of raw binary

--- src/ext/b/decoder.sv
+++ src/ext/b/decoder.sv
@@ -10,7 +10,7 @@ module BALUdecoder( input[2:0] funct3
 always @(*)
 
 begin
-  b_alu_control = 4'b0000; // default to 0 for non-zba/b instructions
+  b_alu_control = B_ALU_CTRL__NONE; // default to NONE for non-zba/b instructions
   case (alu_op)
     6'b011011:
     begin
@@ -18,17 +18,17 @@ begin
         7'b0000100: // zba module
         begin
           case (funct3)
-            3'b010: b_alu_control = 4'b0001; // sh1add
-            3'b100: b_alu_control = 4'b0010; // sh2add
-            3'b110: b_alu_control = 4'b0011; // sh3add
+            3'b010: b_alu_control = B_ALU_CTRL__SH1ADD; // sh1add
+            3'b100: b_alu_control = B_ALU_CTRL__SH2ADD; // sh2add
+            3'b110: b_alu_control = B_ALU_CTRL__SH3ADD; // sh3add
           endcase
         end
         7'b0100000: // zbb module
         begin
           case (funct3)
-            3'b111: b_alu_control = 4'b0100; // andn
-            3'b110: b_alu_control = 4'b0101; // orn
-            3'b100: b_alu_control = 4'b0110; // xnor
+            3'b111: b_alu_control = B_ALU_CTRL__ANDN; // andn
+            3'b110: b_alu_control = B_ALU_CTRL__ORN; // orn
+            3'b100: b_alu_control = B_ALU_CTRL__XNOR; // xnor
           endcase
         end
 
--- src/types.svh
+++ src/types.svh
@@ -63,4 +63,15 @@ typedef enum logic [1:0]
   , PC_SRC__ALU_RESULT = 2'b10
   } pc_src_t;
 
+
+typedef enum logic [3:0]
+  { B_ALU_CTRL__NONE   = 4'b0000
+  , B_ALU_CTRL__SH1ADD = 4'b0001
+  , B_ALU_CTRL__SH2ADD = 4'b0010
+  , B_ALU_CTRL__SH3ADD = 4'b0011
+  , B_ALU_CTRL__ANDN   = 4'b0100
+  , B_ALU_CTRL__ORN    = 4'b0101
+  , B_ALU_CTRL__XNOR   = 4'b0110
+  } b_alu_control_t;
+
 `endif
