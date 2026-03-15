64125c469cb588edda9e36e04eafcc86f0ec3908
Boris <boris.potapov@mail.utoronto.ca>
2026-03-15 04:40:25 +0000

    align

--- src/ext/b/decoder.sv
+++ src/ext/b/decoder.sv
@@ -11,32 +11,32 @@ module BALUdecoder
   localparam bit [6:0] FUNCT7_ZBA = 7'b0010000;
   localparam bit [6:0] FUNCT7_ZBB = 7'b0100000;
 
-always_comb
-  case (opcode)
-    7'b0110011:
-      case (funct7)
-        FUNCT7_ZBA:
-          case (funct3)
-            3'b010:  b_alu_control = B_ALU_CTRL__SH1ADD;
-            3'b100:  b_alu_control = B_ALU_CTRL__SH2ADD;
-            3'b110:  b_alu_control = B_ALU_CTRL__SH3ADD;
-            default: b_alu_control = B_ALU_CTRL__NONE;
-          endcase
+  always_comb
+    case (opcode)
+      7'b0110011:
+        case (funct7)
+          FUNCT7_ZBA:
+            case (funct3)
+              3'b010:  b_alu_control = B_ALU_CTRL__SH1ADD;
+              3'b100:  b_alu_control = B_ALU_CTRL__SH2ADD;
+              3'b110:  b_alu_control = B_ALU_CTRL__SH3ADD;
+              default: b_alu_control = B_ALU_CTRL__NONE;
+            endcase
 
-        FUNCT7_ZBB:
-          case (funct3)
-            3'b111:  b_alu_control = B_ALU_CTRL__ANDN;
-            3'b110:  b_alu_control = B_ALU_CTRL__ORN;
-            3'b100:  b_alu_control = B_ALU_CTRL__XNOR;
-            default: b_alu_control = B_ALU_CTRL__NONE;
-          endcase
+          FUNCT7_ZBB:
+            case (funct3)
+              3'b111:  b_alu_control = B_ALU_CTRL__ANDN;
+              3'b110:  b_alu_control = B_ALU_CTRL__ORN;
+              3'b100:  b_alu_control = B_ALU_CTRL__XNOR;
+              default: b_alu_control = B_ALU_CTRL__NONE;
+            endcase
 
-        // TODO: Implement zbs into ALU decoder, also confirm what zbb instructions are being implemented.
-        default: b_alu_control = B_ALU_CTRL__NONE;
+          // TODO: Implement zbs into ALU decoder, also confirm what zbb instructions are being implemented.
+          default: b_alu_control = B_ALU_CTRL__NONE;
 
-      endcase
+        endcase
 
-    default: b_alu_control = B_ALU_CTRL__NONE;
-  endcase
+      default: b_alu_control = B_ALU_CTRL__NONE;
+    endcase
 
 endmodule
