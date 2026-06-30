a23f3becc213b2124854ed52a752b67a1b429cee
SenseiPrimexus <kevin20383sabis@gmail.com>
2026-06-30 12:26:03 -0400

    Refactor utoss_riscv: moved CSR registers to decode stage

--- src/utoss_riscv.sv
+++ src/utoss_riscv.sv
@@ -43,11 +43,6 @@ module utoss_riscv
 
   data_t      wb_result;
   logic [4:0] wb_rd;
-  csr_addr_t  csr_addr_d;
-  data_t      csr_read_data;
-  // Keep the live CSR read address referenced for lint when CSR is gated off.
-  wire        unused_csr_addr_d = &{1'b0, csr_addr_d};
-
   // common declarations end
 
   // fetch stage start (@thatlittlegit)
@@ -85,11 +80,12 @@ module utoss_riscv
     , .clk         ( clk                     )
     , .reset       ( reset                   )
     , .data        ( wb_result               )
-    , .csr_read_data( csr_read_data          )
+    , .csr_write_addr( mem_to_wb_reg.csr_addr        )
+    , .csr_write_enable_wb( mem_to_wb_reg.csr_write_enable )
+    , .csr_write_data_wb( mem_to_wb_reg.csr_write_data )
     , .rd_wb       ( wb_rd                   )
     , .reg_write_w ( mem_to_wb_reg.reg_write )
 
-    , .csr_addr ( csr_addr_d )
     , .rs1 ( id_rs1 )
     , .rs2 ( id_rs2 )
 
@@ -97,20 +93,6 @@ module utoss_riscv
     );
 
   // decode stage end
-
-`ifdef UTOSS_RISCV__ZICSR_ENABLED
-  CSRFile u_csr_file
-    ( .read_addr       ( csr_addr_d                    )
-    , .write_addr      ( mem_to_wb_reg.csr_addr        )
-    , .clk             ( clk                          )
-    , .reset           ( reset                        )
-    , .csr_write_enable( mem_to_wb_reg.csr_write_enable )
-    , .data_in         ( mem_to_wb_reg.csr_write_data   )
-    , .data_out        ( csr_read_data                )
-    );
-`else
-  assign csr_read_data = data_t'(0);
-`endif
 
   // execute stage begin (@MSh-786 and tandr3w)
 
