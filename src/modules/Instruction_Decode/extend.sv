8a1221e54f38b0fcd9e828cb98ba5b3596dd4532
briann-bui <134913555+briann-bui@users.noreply.github.com>
2026-07-22 21:11:53 +0700

    Fix extend lint and Quartus source list

--- src/modules/Instruction_Decode/extend.sv
+++ src/modules/Instruction_Decode/extend.sv
@@ -8,18 +8,15 @@ module extend
   , output imm_t    imm_ext
   );
 
-  always_comb begin
+  always_comb
     case (opcode)
-      OPCODE_OP_IMM,
-      OPCODE_LOAD,
-      OPCODE_JALR:   imm_ext = {{20{instr[31]}}, instr[31:20]};
+      OPCODE_OP_IMM, OPCODE_LOAD, OPCODE_JALR:
+        imm_ext = {{20{instr[31]}}, instr[31:20]};
       OPCODE_STORE:  imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
       OPCODE_BRANCH: imm_ext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
       OPCODE_JAL:    imm_ext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
-      OPCODE_AUIPC,
-      OPCODE_LUI:    imm_ext = {instr[31:12], 12'b0};
+      OPCODE_AUIPC, OPCODE_LUI: imm_ext = {instr[31:12], 12'b0};
       default:       imm_ext = 32'b0;
     endcase
-  end
 
 endmodule
