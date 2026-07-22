782eb07af259849e2336e5614f64f8bb56ed17fe
briann-bui <134913555+briann-bui@users.noreply.github.com>
2026-07-22 20:49:35 +0700

    Extract extend module from instruction decoder

--- src/modules/Instruction_Decode/Instruction_Decode.sv
+++ src/modules/Instruction_Decode/Instruction_Decode.sv
@@ -118,21 +118,11 @@ module Instruction_Decode
     endcase
   end
 
-  // case statement for choosing 32-bit immediate format; based on opcode
-    // this is essentially the extend module of the processor
-  always @(*) begin
-    case (opcode)
-      OPCODE_OP_IMM : imm_ext = {{20{instr[31]}}, instr[31:20]};
-      OPCODE_LOAD  : imm_ext = {{20{instr[31]}}, instr[31:20]};
-      OPCODE_JALR : imm_ext = {{20{instr[31]}}, instr[31:20]};
-      OPCODE_STORE       : imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
-      OPCODE_BRANCH       : imm_ext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-      OPCODE_JAL       : imm_ext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
-      OPCODE_AUIPC  : imm_ext = {instr[31:12], 12'h000};
-      OPCODE_LUI  : imm_ext = {instr[31:12], 12'h000};
-      default:     imm_ext = 32'b0;
-    endcase
-  end
+  extend u_extend
+    ( .instr   ( instr[31:7] )
+    , .opcode  ( opcode      )
+    , .imm_ext ( imm_ext     )
+    );
 
   //Instantiate ALU Decoder module
 
